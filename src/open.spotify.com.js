f39d7fc002d110ce2d7d77e0c9e660236b7b06b5
Cologler <skyoflw@gmail.com>
2019-02-26 03:30:19 +0800

    site open.spotify.com

---
+++ src/open.spotify.com.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name                Sitext: open.spotify.com
+// @namespace           https://github.com/Cologler/
+// @version             0.1
+// @description         try to take over the world!
+// @author              Cologler (skyoflw@gmail.com)
+// @match               https://open.spotify.com/*
+// @grant               GM_registerMenuCommand
+// @grant               GM_download
+// @noframes
+// @license             MIT
+// ==/UserScript==
+
+// hosting on:
+
+// let type script auto-completion work.
+(function() { function require(){}; require("greasemonkey"); })();
+
+(function() {
+    'use strict';
+
+    GM_registerMenuCommand('download cover art', () => {
+        const bgi = document.querySelectorAll('.cover-art-image')[0].style.backgroundImage;
+        function url(v) {
+            return v;
+        }
+        const bgiurl = eval(bgi);
+        const artist = document.querySelector('.mo-meta').textContent;
+        const album = document.querySelector('.mo-info-name').textContent;
+        const fileName = `${artist} - ${album}.jpg`;
+        GM_download(bgiurl, fileName);
+    });
+})();
